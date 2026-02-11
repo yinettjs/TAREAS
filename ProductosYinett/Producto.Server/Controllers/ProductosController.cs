ac47e43973ed1a1d82f70a5b041579db836ecda0
yinettjs <marisolsen10@gmail.com>
2026-02-10 20:55:28 -0400

    Crud yinet

---
+++ ProductosYinett/Producto.Server/Controllers/ProductosController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Producto.Server.Models;
+
+namespace Producto.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ProductosContext _context;
+
+        public ProductosController(ProductosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public async Task<IActionResult> CrearProducto(Productoss producto)
+        {
+            //guardar el producto en la base de datos
+            await _context.Productos.AddAsync(producto);
+            await _context.SaveChangesAsync();
+
+            //devolver un mensaje de exito
+            return Ok();
+        }
+        [HttpGet]
+        [Route("listaProducto")]
+        public async Task<ActionResult<IEnumerable<Productoss>>> GetProducts()
+        {
+            //Obten la lista de productos de la base de datos
+            var productos = await _context.Productos.ToListAsync();
+
+            //devuelve una lista de productos
+            return Ok(productos);
+        }
+
+        [HttpGet]
+        [Route("verProducto")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            //obtener el producto de la base de datos
+            Productoss producto = await _context.Productos.FindAsync(id);
+
+            //devolver el producto
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        [HttpPut]
+        [Route("editarProducto")]
+        public async Task<IActionResult> UpdateProduct(int id, Productoss producto)
+        {
+            //Actualizar el producto en la base de datos
+            var productoExistente = await _context.Productos.FindAsync(id);
+            productoExistente!.Nombre = producto.Nombre;
+            productoExistente.Descripcion = producto.Descripcion;
+            productoExistente.Precio = producto.Precio;
+
+            await _context.SaveChangesAsync();
+
+            //devolver un mensaje de exito
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("eliminarProducto")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            //Eliminar producto de la base de datos
+            var productoBorrado = await _context.Productos.FindAsync(id);
+            _context.Productos.Remove(productoBorrado!);
+
+            await _context.SaveChangesAsync();
+
+            //Devolver un mensaje de exito
+            return Ok();
+        }
+
+    }
+}
---
+++ ProductosYinett/Producto.Server/Models/ProductosContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Producto.Server.Models
+{
+    public class ProductosContext : DbContext
+    {
+        public ProductosContext(DbContextOptions<ProductosContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Productoss> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Productoss>().HasIndex(c => c.Nombre).IsUnique();
+        }
+    }
+}
